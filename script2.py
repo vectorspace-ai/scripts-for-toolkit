40a833358187d8faef997a6e32664ff7c9f4f3aa
Magniko <magniko1524@gmail.com>
2019-11-25 21:48:53 +0800

    new stuff

--- script2.py
+++ script2.py
@@ -7,11 +7,12 @@ part of the script work.
 One "notable tweak I did was to include the root symbol AND the correlated symbol in each dict entry,
 so pairing them together in a graph would be easier. If it's possible to do it without this....
 well...more data is usually always better.
+
+I noticed a peculiarity when I had an issue with getting the script to display the correct depth level
+for the initial symbols found to have correlation: even if the standard 
 I will also try to make it so that a user can limit the branches, where only the top n branches(n being
 the number branches desired to be displayed) are displayed.
-This is on hold, because the current issue to me is that the script has a problem properly caluculating the
-current depth level it is currently in for some reason. Thus it produces wrong depth keys in the dictionary
-entries. My plan is to use the depth keys to iterate through each dict entry depending on the depth
+My plan is to use the depth keys to iterate through each dict entry depending on the depth
 and then remove the entries within a specific depth that doesn't fall within the top n scores.
 
 I really hope you can all understand my retarded way of explaining. If not, hit me up on Telegram for this
@@ -22,8 +23,7 @@ in the top n(n here again being the max number of nodes desired to be displayed)
 Of course I could be wrong on all of this, and you guys know of a better way to do it. 
 """
 import sys
-import pip
-import pandas as pd
+import csv
 import json
 
 SCORE = 0.01
@@ -31,7 +31,7 @@ SCORE = 0.01
 if len(sys.argv) != 5:
 	print('*****')
 	print('Usage: print script.py <symbol> <depth> <branches> <file_name>')
-	print('Example: python script.py abc 3 2 test.csv')
+	print('Example: python script.py abc 3 test.csv')
 	print('*****')
 	exit()
 
@@ -40,46 +40,40 @@ max_depth = int(sys.argv[2])
 branches = int(sys.argv[3])
 file_name = sys.argv[4]
 
-#this reads the dataset(.csv format) to a 2D list
-df=pd.read_csv(file_name, index_col=0)
+headers = []
+data = {}
+with open(file_name) as file:
+	csv_reader = csv.reader(file, delimiter=',')
+	headers = next(csv_reader)[1:]
+	for row in csv_reader:
+		data[row[0]] = [float(x) for x in row[1:]]
 
-def get_intersected(symbol, df, depth=1, branches=1):
-	result=[]
-	for index, i in enumerate(df.columns):
-		if df.loc[i][symbol] > SCORE:
-			result.append({'root_symbol':symbol, 'cor_symbol':df.columns[index], 'score':df.loc[symbol][i], 'depth':depth, 'branches':branches})
-	return result
+def get_intersected(symbol, dict, depth=1):
+	return [{'root_symbol':symbol, 'cor_symbol':headers[i], 'score':score, 'depth':depth} for i, score in enumerate(dict[symbol]) if score > SCORE]
 
-result = get_intersected(root_symbol, df, branches)
-
+result = get_intersected(root_symbol, data)
 
 if max_depth > 1:
 	#initialize 2 lists containing symbols
 	symbols=[]
 	covered_symbols=[]
-
-	#this is used for the while loop
-	remainer_depth=max_depth
-
+	#this is used for the while loop(subtracted by 1 because first level has already been covered above)
+	remainer_depth=max_depth-1
 	while remainer_depth>0:
 		#removes all symbols previously covered
 		symbols=[x for x in symbols if x not in covered_symbols]
-
 		for j in range(len(result)):
 			#checking if the symbol in question has already been covered or not
 			if(result[j]['cor_symbol'] not in covered_symbols and result[j]['cor_symbol'] not in symbols):
 				symbols.append(result[j]['cor_symbol'])
-
 		for name in symbols:
 			#performs the same function as with the root symbol
-			result[(len(result)):]=(get_intersected(name, df, max_depth-remainer_depth+1, branches))
+			result[(len(result)):]=(get_intersected(name, data, max_depth-remainer_depth+1))
 		remainer_depth-=1
-
 		#updates symbols already covered
 		covered_symbols.extend(symbols)
 
 #if branches > 1:
-
 
 with open('output.json', 'w') as outfile:
 	json.dump(result, outfile)
